a457f0496a0aee1300a76404b8d011a3a41fcbca
威廷的天選四\v60ca <williewu0908@gmail.com>
2024-12-05 00:11:22 +0800

    add check_balance

--- shopping/shoppingCart.py
+++ shopping/shoppingCart.py
@@ -20,6 +20,7 @@ class ShoppingCart:
     def view_cart(self):
         if not self.shopping_cart:
             print("\n購物車是空的。")
+            return 0
         else:
             print("\n購物車內容:")
             total_price = 0
@@ -27,6 +28,7 @@ class ShoppingCart:
                 print(f"{item['name']} - ${item['price']}")
                 total_price += item['price']
             print(f"\n總價格: ${total_price}")
+            return total_price
 
     def checkout(self):
         if not self.shopping_cart:
@@ -44,41 +46,64 @@ class ShoppingCart:
                 self.shopping_cart.clear()
                 print(f"\n付款成功！剩餘餘額: ${self.user_balance}")
 
-    # def checkBalance(self):
-    #     if self.user_balance > 0:
-    #         print(f"\n剩餘餘額: ${self.user_balance}")
-    #     else:
-    #         print("\n餘額不足")
+    def check_balance(self, user_balance, total_price):
+        if total_price > user_balance:
+            return "餘額不足"
+        else:
+            user_balance -= total_price
+            return user_balance
+        
+    def remove_from_cart(self, product_id):
+        item_to_remove = None
+        for item in self.shopping_cart:
+            if item['id'] == product_id:
+                item_to_remove = item
+                break
+        if item_to_remove:
+            self.shopping_cart.remove(item_to_remove)
+            print(f"\n{item_to_remove['name']} 已從購物車中刪除。")
+        else:
+            print("產品編號不存在於購物車內")    
+        
+        
 
 if __name__ == '__main__':
     cart = ShoppingCart()
 
     while True:
-      print("\n選項:\n")
-      print("1. 加入商品到購物車")
-      print("2. 顯示購物車內容")
-      print("3. 付款")
-      print("4. 離開")
-    #   print("5. 查看餘額")
+        print("\n選項:\n")
+        print("1. 加入商品到購物車")
+        print("2. 顯示購物車內容")
+        print("3. 付款")
+        print("4. 離開")
+        print("5. 查看餘額")
 
-      choice = input("\n請輸入選項 (1/2/3/4): ")
+        choice = input("\n請輸入選項 (1/2/3/4/5): ")
 
-      if choice == "1":
-          product_id = int(input("\n請輸入要加入購物車的商品編號: "))
-          cart.add_to_cart(product_id)
+        if choice == "1":
+            product_id = int(input("\n請輸入要加入購物車的商品編號: "))
+            cart.add_to_cart(product_id)
 
-      elif choice == "2":
-          cart.view_cart()
+        elif choice == "2":
+            cart.view_cart()
 
-      elif choice == "3":
-          cart.checkout()
+        elif choice == "3":
+            cart.checkout()
 
-      elif choice == "4":
-          print("\n謝謝光臨，再見！")
-          break
+        elif choice == "4":
+            print("\n謝謝光臨，再見！")
+            break
 
-    #   elif choice == "5":
-    #       cart.checkBalance()
+        elif choice == "5":
+            total_price = cart.view_cart()
+            if total_price == 0:
+                print("\n購物車是空的，無需檢查餘額。")
+            else:
+                balance_result = cart.check_balance(cart.user_balance, total_price)
+                if isinstance(balance_result, str):
+                    print(balance_result)
+                else:
+                    print(f"剩餘餘額: ${balance_result}")
 
-      else:
-          print("\n無效的選項，請重新輸入。")
+        else:
+            print("\n無效的選項，請重新輸入。")
